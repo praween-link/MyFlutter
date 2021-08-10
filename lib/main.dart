77c3a5010f6d3337ec35b6371bdddc433306f685
praween124 <pkumar.link@gmail.com>
2021-08-10 21:01:19 +0530

    Flutter-7 commit : fonts and login page design

--- lib/main.dart
+++ lib/main.dart
@@ -22,45 +22,90 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                width: 100,
-                height: double.infinity,
-                color: Colors.red,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  ),
-                ],
-              ),
-              Container(
-                width: 100,
-                height: double.infinity,
-                color: Colors.blue,
-              ),
-            ],
-          ),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      ),
-    );
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.blueGrey.shade100,
+                  backgroundImage: AssetImage('images/iam.png'),
+                ),
+                Text(
+                  'Praween Kumar',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'MonteCarlo'),
+                ),
+                Text(
+                  'SOFTWEAR DEVELOPER',
+                  style: TextStyle(
+                      color: Colors.white60,
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Cairo',
+                      letterSpacing: 5.0),
+                ),
+                Container(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.grey.shade800,
+                        size: 30.0,
+                      ),
+                      SizedBox(width: 10.0),
+                      Text(
+                        '+91 850 776 6280',
+                        style: TextStyle(
+                            color: Colors.grey.shade900,
+                            fontSize: 20.0,
+                            fontFamily: 'Cairo'),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.grey.shade800,
+                        size: 30.0,
+                      ),
+                      SizedBox(width: 10.0),
+                      Text(
+                        'pkumar.link@gamil.com',
+                        style: TextStyle(
+                          color: Colors.grey.shade900,
+                          fontSize: 20.0,
+                          fontFamily: 'Cairo',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
