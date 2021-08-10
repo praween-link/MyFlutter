112fa4d5b2fdeeb533599ce9c33913c98a136e79
praween124 <pkumar.link@gmail.com>
2021-08-10 12:53:01 +0530

    Flutter-4 commit : Container, SafeArea, rotation, margin, padding

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,12 @@
+//////////////////////////////////
+/// Icons : https://icons8.com/ 
+///
+/// Design Images : https://www.canva.com/design/DAEKpZ6GAO8/LnuM1P5tikAocuurR8BCIg/edit
+///
+///
+//////////////////////////////////
+
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,53 +27,24 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
+      home: Scaffold(
+        backgroundColor: Colors.indigo[200],
+        body: SafeArea(
+          child: Container(
+            height: 100,
+            width: 100,
+            // margin: EdgeInsets.all(8.0),
+            // margin: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+            margin: EdgeInsets.fromLTRB(60, 20, 30, 50), //(left, top, right, bottom)
+            padding: EdgeInsets.all(20.0),
+            color: Colors.indigo[100],
+            child: Text("TCS"),
 
-  bool tcs = false;
-
-  void welcome() {
-    setState(() {
-      if (tcs) {
-        tcs = false;
-      } else {
-        tcs = true;
-      }
-    });
-  }
-
-  Widget MyHomePage() {
-    return Scaffold(
-      backgroundColor: Colors.blueGrey[500],
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey[700],
-        title: Text("Testing App"),
-      ),
-      body: Center(
-        child: Stack(
-          alignment: Alignment.bottomCenter,
-          children: <Widget>[
-            Image(
-              image: AssetImage('images/diamond2.png'),
-            ),
-            Text(
-              tcs ? "Diamond" : "",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 45.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
+            // transform: Matrix4.rotationX(1.0),
+            // transform: Matrix4.rotationY(1.0),
+            transform: Matrix4.rotationZ(0.5),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blueGrey[700],
-        child: Image(
-          image: AssetImage('images/diamond.png'),
-        ),
-        onPressed: () => welcome(),
       ),
     );
   }
