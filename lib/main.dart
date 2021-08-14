c2586e3c617ab4f7e8b2ca0f23ed737a26552d5c
praween124 <pkumar.link@gmail.com>
2021-08-14 22:12:35 +0530

    Flutter-13 commit : Quiz App

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
-import 'package:english_words/english_words.dart';
+import 'package:testing_app/questionAnswer.dart';
+// import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(
@@ -10,10 +10,7 @@ void main() {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: Text("Generate Pair Words"),
-        ),
+        backgroundColor: Colors.blueGrey.shade900,
         body: SafeArea(
           child: MyApp(),
         ),
@@ -29,28 +26,117 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var wordPair = new WordPair.random();
+  List<Icon> scoreKeeper = [];
 
-  void pairWords() {
-    setState(() {
-      wordPair = new WordPair.random();
-    });
-  }
+  // List<String> questions = [
+  //   "Codding is good for developers?",
+  //   'Developers develop new things?',
+  //   'Create mobile application using only java programming language?',
+  // ];
+  // List<bool> answers = [true, true, false];
+
+  List<Questions> questionBank = [
+    Questions(q: 'Codding is good for developers?', a: true),
+    Questions(q: 'Developers develop new things?', a: true),
+    Questions(q: 'Create mobile application using only java programming language?', a: false),
+  ];
+
+  int question_number = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      // child: Text(all.last), // nouns.first
-      child: TextButton(
-        onPressed: () => pairWords(),
-        child: Text(
-          wordPair.asPascalCase,
-          style: TextStyle(
-            fontSize: 30.0,
-            color: Colors.white,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Center(
+              child: Text(
+                questionBank[question_number].questionText,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                ),
+              ),
+            ),
           ),
         ),
-      ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.green,
+              child: Text(
+                "True",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  bool user_answer = questionBank[question_number].answerBool;
+                  user_answer
+                      ? scoreKeeper.add(Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        ))
+                      : scoreKeeper.add(Icon(
+                          Icons.close,
+                          color: Colors.red,
+                        ));
+                  question_number++;
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                "False",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  bool user_answer = questionBank[question_number].answerBool;
+                  !user_answer
+                      ? scoreKeeper.add(Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        ))
+                      : scoreKeeper.add(Icon(
+                          Icons.close,
+                          color: Colors.red,
+                        ));
+                  question_number++;
+                });
+              },
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: scoreKeeper,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/questionAnswer.dart
@@ -0,0 +1,10 @@
+
+class Questions {
+  String questionText;
+  bool answerBool;
+
+  Questions({String q, bool a}){
+    questionText = q; 
+    answerBool = a;
+  }
+}
